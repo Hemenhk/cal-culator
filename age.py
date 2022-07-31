075f17db94aad5770c0685685b64bc7a3dc56b52
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-07-31 16:16:13 +0000

    add: Create a function for men's age

---
+++ age.py
@@ -0,0 +1,9 @@
+def age_men_bmr(age):
+    """
+    This function calculates the age variable
+    which is going to be used in calculating
+    the male user's bmr
+    """
+    #print("This is your age variable used in calculating your bmr\n ")
+    ages_bmr_men = age * 5.677
+    return ages_bmr_men
