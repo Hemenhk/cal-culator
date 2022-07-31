913d970c329058c25cddebd6a2e955e5b529cece
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-07-31 16:14:14 +0000

    add: Create a function for women's weight variable

--- weight.py
+++ weight.py
@@ -8,3 +8,14 @@ def weight_men_bmr(weight):
     
     weights_bmr_men = weight * 13.397
     return weights_bmr_men
+
+def weight_women_bmr(weight):
+    """
+    This function calculates the weight variable
+    which is going to be used in calculating
+    the female user's bmr
+    """
+    #print("This is your weight variable used in calculating your bmr\n ")
+    
+    weights_bmr_women = weight * 9.247
+    return weights_bmr_women
