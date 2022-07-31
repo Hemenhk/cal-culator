c6d0931649da01c877a7373178822b68b00f13a4
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-07-31 16:15:13 +0000

    add: Create a function for men's height

---
+++ height.py
@@ -0,0 +1,9 @@
+def height_men_bmr(height):
+    """
+    This function calculates the height variable
+    which is going to be used in calculating
+    the male user's bmr
+    """
+    #print("This is your height variable used in calculating your bmr\n ")
+    heights_bmr_men = height * 4.799
+    return heights_bmr_men
