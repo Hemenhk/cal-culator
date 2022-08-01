e4f172a6f3933dbe0e08015d024e6e95f042d3f2
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-08-01 09:27:14 +0000

    add: Add additional print statements guiding the user through the app

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ This is an app that calculates the basal metabolic rate (BMR), which is caloric 
 This is a simplistic calculation that takes a few different variables: height, weight and age of the user, and then calculates their BMR. 
 
 [Live Version Of The App](https://cal-culators.herokuapp.com/)
+
+## How to use
+
+CalCulator consists of four different inputs, where the user enters some personal data which will be used to calculate their BMR. The data is age in years, weight in kilograms, height in centimetres and their gender (male or female).
+
+## Features
--- run.py
+++ run.py
@@ -12,17 +12,28 @@ def main():
     of either a male or female user. 
     """
 
+    print("Welcome to CalCulator")
+
+    print("""
+On this app you'll be able to find your Basal Metabolic Rate, or BMR.
+By typing in personal values of your height, age, weight and gender,
+we will calculate your BMR so that you can either increase your calories 
+to build muscle, or to decrease your calories to burn fat.\n""")
+
+    print("All values entered must be numeric values such as '1', '2' '3'\n")
+
     try:
-        age = float(input("what's your age in years?:\n "))
-        print(f"you are {age} years old\n")
+        age = float(input("What's your age in years?:\n "))
+        print(f"You are {age} years old\n")
 
-        height = float(input("what's your height in cm?:\n "))
-        print(f"you are {height} cm tall\n")
+        height = float(input("What's your height in cm?:\n "))
+        print(f"You are {height} cm tall\n")
 
-        weight = float(input("what's your weight in kg?:\n"))
-        print(f"you weigh {weight} kg\n")
+        weight = float(input("What's your weight in kg?:\n"))
+        print(f"You weigh {weight} kg\n")
 
-        print("if you are male enter '1', if you are female enter any other value: \n ")
+        print("""If you are male enter '1'. 
+If you are female enter any other numeric value: \n """)
         gender = int(input("What is your gender?:\n "))
     except ValueError:
         print("Invalid value. You must type a numeric value")
@@ -33,7 +44,7 @@ def main():
         bmr = round(88.362 + weight_men_bmr(weight) + height_men_bmr(height) - age_men_bmr(age))
     else:
          bmr = round(447.593 + weight_women_bmr(weight) + height_women_bmr(height) - age_women_bmr(age))
-    print(f"Your bmr is {bmr}\n ")
+    print(f"Your bmr is {bmr} calories\n ")
 
     print("Calculation finished.")
 
